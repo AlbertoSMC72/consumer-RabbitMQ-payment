049c6b1e4cc0345a4bab869e4f317eb3ec7630d8
AlbertoSMC72 <223247@ids.upchiapas.edu.mx>
2024-03-01 00:00:02 -0600

    consumidor preprado para enviar mensajes de un broker a una api

---
+++ app.ts
@@ -0,0 +1,44 @@
+import amqp from "amqplib/callback_api";
+
+async function connect() {
+    try {
+        await amqp.connect(
+            "amqp://34.193.221.88",
+            (err: any, conn: amqp.Connection) => {
+                if (err) throw new Error(err);
+                conn.createChannel((errChanel: any, channel: amqp.Channel) => {
+                    if (errChanel) throw new Error(errChanel);
+                    channel.assertQueue();
+                    channel.consume("cola1", async (data: amqp.Message | null) => {
+                        if (data?.content !== undefined) {
+                            console.log(`Recibido nuevo producto: ${data.content}`);
+                            const content = data?.content;
+                            const parsedContent = JSON.parse(content.toString());
+                            const headers = {
+                                "Content-Type": "application/json",
+                            };
+                            const body = {
+                                method: "POST",
+                                headers,
+                                body: JSON.stringify(parsedContent),
+                            };
+                            console.log(parsedContent);
+                            /* fetch("http://localhost:8000/payment", body)
+                                .then(() => {
+                                    console.log("datos enviados");
+                                })
+                                .catch((err: any) => {
+                                    throw new Error(err);
+                                }); */
+                            await channel.ack(data);
+                        }
+                    });
+                });
+            }
+        );
+    } catch (err: any) {
+        throw new Error(err);
+    }
+}
+
+connect();
