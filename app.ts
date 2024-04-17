d7886a0c81a68dd5f5a309e9497122f0c1ada289
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-04-17 07:30:38 -0600

    Actualiza la URL de la solicitud de pago y prepara el consumidor para enviar mensajes de un broker a una API

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import amqp from "amqplib/callback_api";
+import fetch from "node-fetch"; 
 
 async function connect() {
     try {
@@ -8,7 +9,7 @@ async function connect() {
                 if (err) throw new Error(err);
                 conn.createChannel((errChanel: any, channel: amqp.Channel) => {
                     if (errChanel) throw new Error(errChanel);
-                    channel.assertQueue();
+                    channel.assertQueue("cola1"); // Debes especificar la cola
                     channel.consume("cola1", async (data: amqp.Message | null) => {
                         if (data?.content !== undefined) {
                             console.log(`Recibido nuevo producto: ${data.content}`);
@@ -23,21 +24,21 @@ async function connect() {
                                 body: JSON.stringify(parsedContent),
                             };
                             console.log(parsedContent);
-                            fetch("http://44.219.166.176:8000/payment", body)
-                                .then(() => {
-                                    console.log("datos enviados");
-                                })
-                                .catch((err: any) => {
-                                    throw new Error(err);
-                                }); 
-                            await channel.ack(data);
+                            try {
+                                await fetch("http://44.219.166.176:8000/payment", body);
+                                console.log("datos enviados");
+                                await channel.ack(data); // Acknowledge solo después de que se ha enviado correctamente
+                            } catch (err) {
+                                console.error("Error al enviar datos:", err);
+                                await channel.nack(data); // Reintenta o mueve a una cola de reintentos, según lo deseado
+                            }
                         }
                     });
                 });
             }
         );
-    } catch (err: any) {
-        throw new Error(err);
+    } catch (err) {
+        console.error("Error al conectar:", err);
     }
 }
 
