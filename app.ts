89448f3a79b7d8ac00c59bc6e329720349cf66ed
AlbertoSMC72 <123435927+AlbertoSMC72@users.noreply.github.com>
2024-04-18 12:55:26 -0600

    Actualiza la URL de la solicitud de pago y corrige el nombre de la cola en el consumidor

--- app.ts
+++ app.ts
@@ -4,13 +4,13 @@ import fetch from "node-fetch";
 async function connect() {
     try {
         await amqp.connect(
-            "amqp://34.193.221.88",
+            "amqp://3.216.68.220",
             (err: any, conn: amqp.Connection) => {
                 if (err) throw new Error(err);
                 conn.createChannel((errChanel: any, channel: amqp.Channel) => {
                     if (errChanel) throw new Error(errChanel);
-                    channel.assertQueue("cola1"); // Debes especificar la cola
-                    channel.consume("cola1", async (data: amqp.Message | null) => {
+                    channel.assertQueue("cola_analisis"); 
+                    channel.consume("cola_analisis", async (data: amqp.Message | null) => {
                         if (data?.content !== undefined) {
                             console.log(`Recibido nuevo producto: ${data.content}`);
                             const content = data?.content;
@@ -25,12 +25,12 @@ async function connect() {
                             };
                             console.log(parsedContent);
                             try {
-                                await fetch("http://44.219.166.176:8000/payment", body);
+                                await fetch("http://3.212.95.23:3000/api/analisi", body);
                                 console.log("datos enviados");
-                                await channel.ack(data); // Acknowledge solo después de que se ha enviado correctamente
+                                await channel.ack(data); 
                             } catch (err) {
                                 console.error("Error al enviar datos:", err);
-                                await channel.nack(data); // Reintenta o mueve a una cola de reintentos, según lo deseado
+                                await channel.nack(data); 
                             }
                         }
                     });
